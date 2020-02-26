d1a788cad79f6b84d2ef7f064ff50a8784908c72
Sanntidslaben plass 25 <glemtgitconfig>
2020-02-26 18:48:41 +0100

    network updates

--- Network-go-master/network_prototype.go
+++ Network-go-master/network_prototype.go
@@ -1,18 +1,19 @@
 package main
 import (
-	"./Network-go-master/network/bcast"
-	// "./network/localip"
+	"./network/bcast"
+	"./network/localip"
 	// "./network/peers"
 	// "flag"
 	"fmt"
 	// "os"
 	"time"
+	// "strconv"
 )
 
 var NUMBER_OF_FLOORS int = 4
 
 type ElevatorData struct {
-    // ID          int
+    ID          string
     TimeStamp   int
 	// State		int
 	// Location	int
@@ -23,30 +24,45 @@ type ElevatorData struct {
 }
 
 func main(){
-
+	
+	// Get local IP adress
+	localIP, err := localip.LocalIP() 
+	if err != nil {
+		fmt.Println("Error")
+	}
+	// We could convert IP like this:
+	// splitIP := strings.Split(localIP, ",")
+	// intIP, err := strconv.Atoi(splitIP[3])
+	
 	dataRx := make(chan ElevatorData)
 	dataTx := make(chan ElevatorData)
-
+	
 	sendData := make(chan ElevatorData)
-
-	go bcast.Transmitter(16570, dataTx)
-	go bcast.Receiver(16570, dataRx)
-
+	
+	go bcast.Transmitter(16731, dataTx)
+	go bcast.Receiver(16731, dataRx)
+	
 	// This sends data to network. Test only.
 	// Should replace with real data from main.
 	go func(){
-		info := ElevatorData{0}
+		info := ElevatorData{localIP, 0}
 		for{
 			info.TimeStamp++
 			sendData<- info
 			time.Sleep(time.Second*1)
 		}
 	}()
-
+	
 	for{
 		select{
 		case r := <-dataRx:
-			fmt.Println(r)
+			if r.ID == localIP{
+				// Sent from local computer
+				fmt.Println("Ignore")
+			}else{
+				// From different computer
+				fmt.Println(r)
+			}
 		case s := <-sendData:
 			dataTx<- s
 		}
