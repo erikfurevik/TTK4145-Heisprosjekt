2608a56267c753899befbf05697a0f69edf537c4
siawash naqibi <siawash@live.no>
2020-03-31 20:53:40 +0200

    did some testing

--- driver-go-master/main.go
+++ driver-go-master/main.go
@@ -1,96 +1,30 @@
 package main
 
 import (
-	//"fmt"
-	"strconv"
-	//"time"
 	"os"
+	"fmt"
+	
 
-	"./config"
-	"./elevio"
-	"./fsm"
-	ec "./ElevatorController"
-	nc"./NetworkController"
-	bcast "./network/bcast"
-	peers "./network/peers"
 )
 
 
 func main() {
-	// For å kjøre koden må du først skrive "run go main.go" som vanlig så må du spesifisere IDen til heisen og hvilke port den skal kobles til
-	// på simulatoren
-	//eksempel kode: go run main.go 0 15000
-	//koden kjører med id = 0 og port = 15000
-	 
-	LocalIDString := os.Args[1]
-	localhost := "localhost:" + os.Args[2]
-	LocalID,err := strconv.Atoi(LocalIDString)
-
-	if err != nil {
-		panic(err.Error())
-	}
+	data := make([]byte, 1024)
+	readFile, _ := os.Open("cabOrders")
 	
-	//defualt := "localhost:15657"
-	elevio.Init(localhost, config.NumFloor)
-	channels := fsm.StateChannels{
-		Elevator:       make(chan config.Elev),
-		NewOrder:       make(chan elevio.ButtonEvent, 100),
-		ArrivedAtFloor: make(chan int),
-		DeleteQueue:	make(chan [config.NumFloor][config.NumButtons] bool),
-	}
-
-	network := nc.NetworkChannels{
-		//from network to elevator controller
-	UpdateMainLogic:  			make(chan [config.NumElevator]config.Elev, 100),
-	OnlineElevators: 			make(chan [config.NumElevator]bool),	
-	ExternalOrderToLocal:		make(chan config.Keypress),			
+	//read past data
+	readFile.Read(data) //read the data from file
+	stringData := string(data)
+	fmt.Println(stringData)
 	
-	//from elevator to network controller
-	LocalOrderToExternal:  		make(chan config.Keypress), 			
-	LocalElevatorToExternal:  	make(chan [config.NumElevator]config.Elev), 			
+	writeFile, _ := os.Create("cabOrders")
 	
-	//network controller to network
-	OutgoingMsg:     			make(chan config.Message),		
-	OutgoingOrder: 				make(chan config.Keypress),		
-	PeersTransmitEnable:    	make(chan bool),				
-
-	//network to network controller
-	IncomingMsg:     			make(chan config.Message, 30),			
-	IncomingOrder: 				make(chan config.Keypress),		
-	PeerUpdate:     	 		make(chan peers.PeerUpdate),
-	} 
-
-	var (
-		newOrder = make(chan elevio.ButtonEvent)
-		updateLight = make(chan [config.NumElevator]config.Elev)
-	)
-
-
-	//id_string := strconv.Itoa(LocalID)
-	msgpPort := 42030 //Port for å sende stats
-	orderPort := 42050 //Port for å sende ordre
-	peersPort := 42056 //Port for peers on network
-
-	go elevio.PollButtons(newOrder)
-	go elevio.PollFloorSensor(channels.ArrivedAtFloor)
 	
-	go fsm.RunElevator(channels)
-	go ec.MainLogicFunction(LocalID ,newOrder, updateLight, channels, network)
-	go ec.LightSetter(updateLight,LocalID)
-	go nc.NetworkController(LocalID, network)
-
-
-	go bcast.Transmitter(msgpPort, network.OutgoingMsg)           
-	go bcast.Receiver(msgpPort, network.IncomingMsg)
+	// open input file
 	
-	go bcast.Transmitter(orderPort, network.OutgoingOrder)           
-	go bcast.Receiver(orderPort, network.IncomingOrder)
+	writeFile.WriteString("blaaaaa ")
+	writeFile, _ = os.Create("cabOrders")
+	writeFile.WriteString("YOu know it ")
+	//fi.Close()
 
-
-	go peers.Transmitter(peersPort, LocalIDString, network.PeersTransmitEnable) 
-	go peers.Receiver(peersPort, network.PeerUpdate)             
-
-	select{}
 }
-
-	
